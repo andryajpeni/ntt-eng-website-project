8a415c8211d9a1a4346b76fb91313370cf2fad80
Andry Peni <andryajpeni@engineer.com>
2024-09-14 06:04:51 +0800

    change

--- assets/js/nav.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function (event) {
-  if ($(window).scrollTop() <= 0) {
-    $("#test").attr("src", "./assets/img/logo/NTT-PL-100x48-white.png");
-  } else {
-    $("#test").attr("src", "./assets/img/logo/NTT-PL-100x48.png");
-  }
-});
