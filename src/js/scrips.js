85fb2c05f9be641cc8b9639ccc24d778597eebdf
Andry Peni <andryajpeni@engineer.com>
2025-01-02 01:18:21 +0800

    Update scrips.js

--- src/js/scrips.js
+++ src/js/scrips.js
@@ -94,19 +94,7 @@ xc.addEventListener("click", function () {
 xd.addEventListener("click", function () {
   xmd.classList.toggle("yl_mob-linkbox--open");
 });
-//==>Menu show<==//
-// const x = document.getElementById("navbtn");
-// const s = document.getElementById("megamenu");
-// x.addEventListener("click", function () {
-//   s.classList.toggle("is-open");
-// });
 
-// const a = document.getElementById("yl_nav");
-// window.addEventListener("click", function (close) {
-//   if (!x.contains(close.target) && !a.contains(close.target)) {
-//     s.classList.remove("is-open");
-//   }
-// });
 //==>Hamburger Menu show<==//
 const h = document.querySelector(".hamburger");
 const h1 = document.querySelector("#hamburger-1");
@@ -186,22 +174,154 @@ tabs.forEach((tab, index) => {
     ); // Adjust the speed (600ms) as needed
   });
 });
-//table secarh
-// function cariWarta() {
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("tabs_search");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("myTable");
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
+
+//youtube
+var locations = [
+  "https://www.youtube.com/embed/nc-ZYmFXw0o?si=4hs3I2aMWcUS8MsH",
+  "https://www.youtube.com/embed/DbESZ6pCy5I?si=jzKzx9YVEgYRw9Yy",
+  "https://www.youtube.com/embed/NHnqNz06GyI?si=HWXznY539Ua2SCs5",
+  "https://www.youtube.com/embed/Zai0DnM_MRU?si=MVLjs-U3vLhZ3D1A",
+  "https://www.youtube.com/embed/WgBsub74uqI?si=PE-3CWfrfec8h2ve",
+];
+
+var btnSrc = document.getElementsByClassName("srcChanger");
+for (let i = 0; i < btnSrc.length; i++) {
+  btnSrc[i].addEventListener("click", function () {
+    document.getElementsByClassName("responsive_frame")[0].src = locations[i];
+    document.getElementById("demo").innerHTML = locations[i];
+  });
+}
+//header
+const mainProgressBar = document.querySelector(
+  ".progress-bar--primary .progress-bar__fill"
+);
+const mainPosts = document.querySelectorAll(".main-post");
+const posts = document.querySelectorAll(".post");
+
+let i = 0;
+let postIndex = 0;
+let currentPost = posts[postIndex];
+let currentMainPost = mainPosts[postIndex];
+
+let progressInterval = setInterval(progress, 100);
+
+function progress() {
+  if (i === 100) {
+    i = -5;
+    // reset progress bar
+    currentPost.querySelector(".progress-bar__fill").style.width = 0;
+    mainProgressBar.style.width = 0;
+    currentPost.classList.remove("post--active");
+
+    postIndex++;
+
+    currentMainPost.classList.add("main-post--not-active");
+    currentMainPost.classList.remove("main-post--active");
+
+    // reset postIndex to loop over the slides again
+    if (postIndex === posts.length) {
+      postIndex = 0;
+    }
+
+    currentPost = posts[postIndex];
+    currentMainPost = mainPosts[postIndex];
+  } else {
+    i++;
+    currentPost.querySelector(".progress-bar__fill").style.width = `${i}%`;
+    mainProgressBar.style.width = `${i}%`;
+    currentPost.classList.add("post--active");
+
+    currentMainPost.classList.add("main-post--active");
+    currentMainPost.classList.remove("main-post--not-active");
+  }
+}
+
+posts.forEach((post, index) => {
+  post.addEventListener("click", () => {
+    disablePostsTemporarily();
+    i = 0; // Reset the progress bar
+    postIndex = index;
+    updatePosts();
+  });
+});
+
+function disablePostsTemporarily() {
+  // Disable pointer events on all posts
+  posts.forEach((post) => {
+    post.classList.add("post--disabled");
+  });
+
+  // Re-enable pointer events after 2 1/2 seconds
+  setTimeout(() => {
+    posts.forEach((post) => {
+      post.classList.remove("post--disabled");
+    });
+  }, 2500);
+}
+
+function updatePosts() {
+  // Reset all progress bars and classes
+  posts.forEach((post) => {
+    post.querySelector(".progress-bar__fill").style.width = 0;
+    post.classList.remove("post--active");
+  });
+
+  mainPosts.forEach((mainPost) => {
+    mainPost.classList.add("main-post--not-active");
+    mainPost.classList.remove("main-post--active");
+  });
+
+  // Update the current post and main post
+  currentPost = posts[postIndex];
+  currentMainPost = mainPosts[postIndex];
+
+  currentPost.querySelector(".progress-bar__fill").style.width = `${i}%`;
+  mainProgressBar.style.width = `${i}%`;
+  currentPost.classList.add("post--active");
+
+  currentMainPost.classList.add("main-post--active");
+  currentMainPost.classList.remove("main-post--not-active");
+}
+
+// PAGE HEADER FADE
+const fadeHead = $("#yl-header");
+$(window).on("scroll", function () {
+  const st = $(this).scrollTop();
+  fadeHead.css({ opacity: 1 - st / 400 });
+  fadeHead.css({ "transition-delay": "0s" });
+  fadeHead.css({ transition: "0.01s ease-in-out" });
+});
+//Counting people
+// get counting data element
+var count = document.getElementsByClassName("counter");
+var inc = [];
+
+function intervalFunc() {
+  for (let i = 0; i < count.length; i++) {
+    inc.push(1);
+    //get counting data
+    if (inc[i] != count[i].getAttribute("max-data")) {
+      inc[i]++;
+    }
+    count[i].innerHTML = inc[i];
+  }
+}
+// setInterval(intervalFunc, 100);
+var main = document.getElementById("about"); //get section
+window.onscroll = function () {
+  var timer = setInterval(() => {
+    var topElem = main.offsetTop;
+    var btmElem = main.offsetTop + main.clientHeight;
+    var topScreen = window.pageYOffset;
+    var btmScreen = window.pageYOffset + window.innerHeight;
+    if (btmScreen > topElem && topScreen < btmElem) {
+      intervalFunc();
+    } else {
+      clearInterval(timer);
+      for (let i = 0; i < count.length; i++) {
+        count[i].innerHTML = 1;
+        inc = [];
+      }
+    }
+  }, 50); //speed counting
+};
